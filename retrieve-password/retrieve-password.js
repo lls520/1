1f3500372826e8fbb76c5da86cc077522ab15f80
ZJY <xianxiang188337@163.com>
2022-06-02 17:30:43 +0800

    '前六页面整体提交'

--- retrieve-password/retrieve-password.js
+++ retrieve-password/retrieve-password.js
@@ -1,3 +1,23 @@
-// 手机号
-var shouji = document.getElementsByClassName('shouji')[0];
+var phone = document.getElementById('phone');
+// 验证码
+var yanzheng = document.getElementsByClassName('butt')[0];
+var aa = 15783
+
+function fn() {
+    var i = 5;
+    var ar = setInterval(function() {
+        yanzheng.disabled = true
+        yanzheng.innerHTML = "正在获取(" + i + ")"
+        i--;
+        if (i == -2) {
+            clearInterval(ar);
+            yanzheng.innerHTML = '重新获取'
+            yanzheng.disabled = false;
+            alert(aa)
+        }
+    }, 1000)
+}
+
+
+// 注册按钮
 var butt = document.getElementById('is');
